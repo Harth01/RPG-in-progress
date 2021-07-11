44d7e4778e7af8cf6e1d57a0f6fa6b5ca1164b47
Harth01 <alexandru.dan3107@gmail.com>
2021-07-12 01:59:13 +0300

    RPG

---
+++ Assets/Scripts/HurtEnemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtEnemy : MonoBehaviour
+{
+    public int damageToGive;
+    public GameObject damageBurst;
+    public GameObject damageNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag=="Enemy")
+        {
+            //Destroy(other.gameObject);
+            other.gameObject.GetComponent<EnemyHealthManager>().HurtEnemy(damageToGive);
+            Instantiate(damageBurst, other.transform.position, other.transform.rotation);
+            var clone = (GameObject) Instantiate(damageNumber, other.transform.position, Quaternion.Euler(Vector3.zero));
+            clone.GetComponent<FloatingNumbers>().damageNumber =damageToGive;
+        }
+    }
+    
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    private Animator anim;
+    private bool playerMoving;
+    public Vector2 lastMove;
+    private Rigidbody2D myRigidBody;
+    private static bool playerExists;
+    private bool attacking;
+    public float attackTime;
+    private float attackTimeCounter;
+    void Start()
+    {
+        anim= GetComponent<Animator>();
+        myRigidBody = GetComponent<Rigidbody2D>();
+        if(!playerExists)
+        {
+            playerExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    void Update()
+    {
+        playerMoving = false;
+        if (!attacking)
+        {
+            if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+            {
+                myRigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidBody.velocity.y);
+                //transform.Translate(new Vector3( Input.GetAxisRaw("Horizontal") * moveSpeed *Time.deltaTime,0f,0f));
+                playerMoving = true;
+                lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+            }
+            if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+            {
+                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+                //transform.Translate( new Vector3(0f,Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime , 0f));
+                playerMoving = true;
+                lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+            }
+            if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+            {
+                myRigidBody.velocity = new Vector2(0f, myRigidBody.velocity.y);
+            }
+            if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+            {
+                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, 0f);
+            }
+        
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                attackTimeCounter = attackTime;
+                attacking = true;
+                myRigidBody.velocity = Vector2.zero;
+                anim.SetBool("Attack", true);
+            }
+        }
+        if(attackTimeCounter > 0)
+        {
+            attackTimeCounter -= Time.deltaTime;
+        }
+        if(attackTimeCounter < 0)
+        {
+            attacking = false;
+            anim.SetBool("Attack", false);
+        }
+        anim.SetFloat("x", Input.GetAxisRaw("Horizontal"));
+            anim.SetFloat("y", Input.GetAxisRaw("Vertical"));
+            anim.SetBool("PlayerMoving", playerMoving);
+            anim.SetFloat("LastMoveX", lastMove.x);
+            anim.SetFloat("LastMoveY", lastMove.y);
+    }
+    
+}
